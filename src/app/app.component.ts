d2da6049a15d302cbd4a55fd69b71bc93bf0a194
Peter Ramsing <peter.ramsing@mac.com>
2017-10-13 23:02:48 -0700

    Binds and updates the items with their docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+export interface Item { label: string; done: boolean; }
+export interface ItemId extends Item { id: string; }
 
 @Component({
   selector: 'app-root',
@@ -8,9 +12,23 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items: Observable<any[]>;
+  private itemCollection: AngularFirestoreCollection<Item>;
+  private itemDoc: AngularFirestoreDocument<Item>; // NOTE: Maybe?
+  items: Observable<ItemId[]>;
 
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+  constructor(private afs: AngularFirestore) {
+    this.itemCollection = afs.collection<Item>('items');
+    this.items = this.itemCollection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+  }
+
+  toggleItemComplete(item: ItemId) {
+    this.itemDoc = this.afs.doc<ItemId>(`items/${item.id}`);
+    this.itemDoc.update({done: item.done, label: item.label});
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
@@ -15,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
