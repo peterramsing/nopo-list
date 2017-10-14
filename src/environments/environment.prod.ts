7846655ae63dcf49656eaa72107b8c4c9e4422e2
Peter Ramsing <peter.ramsing@mac.com>
2017-10-14 00:15:42 -0700

    Adds Production Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAXo43_KfiRZC5ofZxW4Eex5NhGwmdFKeY",
+    authDomain: "nopo-list-produc.firebaseapp.com",
+    databaseURL: "https://nopo-list-produc.firebaseio.com",
+    projectId: "nopo-list-produc",
+    storageBucket: "",
+    messagingSenderId: "163555733862"
+  },
 };
